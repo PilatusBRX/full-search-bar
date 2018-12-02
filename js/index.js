00934582f85d7319965969bb0ca85db77e4fbdc3
Marcelo Anselmo <marcelo.anselmo4@gmail.com>
2018-12-01 22:13:51 -0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+//Extend-search 
+$(function() {
+  $('#search-menu').removeClass('toggled');
+
+  $('#search-icon').click(function(e) {
+    e.stopPropagation();
+    $('#search-menu').toggleClass('toggled');
+    $("#popup-search").focus();
+  });
+  
+  $('#search-menu input').click(function(e) {
+    e.stopPropagation();
+  });
+
+  $('#search-menu, body').click(function() {
+    $('#search-menu').removeClass('toggled');
+  });
+});
